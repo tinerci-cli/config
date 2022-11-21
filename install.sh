2eafa4a51ba5f3d7fafefa66a6093f20948723f9
tinerci <pitbull@mr.worldwide.www>
2022-11-21 01:09:20 +0100

    add bad install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+mkdir "$HOME"/code
+ln -s "$HOME"/code/config/htop "$XDG_CONFIG_HOME"
+ln -s "$HOME"/code/config/mpv "$XDG_CONFIG_HOME"
+ln -s "$HOME"/code/config/neofetch "$XDG_CONFIG_HOME"
+ln -s "$HOME"/code/config/nvim "$XDG_CONFIG_HOME"
+ln -s "$HOME"/code/config/shell "$XDG_CONFIG_HOME"
+ln -s "$HOME"/code/config/tmux "$XDG_CONFIG_HOME"
+ln -s "$HOME"/code/config/zsh "$XDG_CONFIG_HOME"
+ln -s "$HOME"/code/config/user-dirs.dirs "$XDG_CONFIG_HOME"
+cd ..
+mv ./config "$HOME"/code
