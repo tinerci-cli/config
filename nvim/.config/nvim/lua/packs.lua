a0b271ec01135fa59d67db6d079835522cb746be
name <gnuraver@freetard.org>
2023-11-12 20:57:54 +0100

    revert to fzf, faster on large directories

--- nvim/.config/nvim/lua/packs.lua
+++ nvim/.config/nvim/lua/packs.lua
@@ -41,7 +41,6 @@ return require('packer').startup(function(use)
 		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-	use 'nvim-telescope/telescope-fzy-native.nvim'
 	use 'nvim-telescope/telescope-file-browser.nvim'
 
 	-- which-key
