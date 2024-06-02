a51211abdb99745710ae26e9b78f1e6cf27fe7f9
mete <mete@localhost.localdomain>
2024-06-02 22:29:03 +0200

    setup jdtls like a boss

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -1,51 +1,52 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 -- to learn how to use mason.nvim
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = { "clangd", "jdtls" },
-  handlers = {
-	    function(jdtls)
-      require('lspconfig')[jdtls].setup({})
-    end,
-  },
+	ensure_installed = { "clangd" },
+	handlers = {
+		--  function(jdtls)
+		--   require('lspconfig')[jdtls].setup({})
+		-- end,
+		jdtls = lsp_zero.noop,
+	},
 })
 local cmp = require('cmp')
 
 cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-  },
-  mapping = {
-    ['<Enter>'] = cmp.mapping.confirm({select = false}),
-    ['<Esc>'] = cmp.mapping.abort(),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-    ['<Tab>'] = cmp.mapping.select_next_item({behavior = 'select'}),
-    ['<C-p>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_prev_item({behavior = 'insert'})
-      else
-        cmp.complete()
-      end
-    end),
-    ['<C-n>'] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_next_item({behavior = 'insert'})
-      else
-        cmp.complete()
-      end
-    end),
-  },
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
+	sources = {
+		{ name = 'nvim_lsp' },
+	},
+	mapping = {
+		['<Enter>'] = cmp.mapping.confirm({ select = false }),
+		['<Esc>'] = cmp.mapping.abort(),
+		['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+		['<Tab>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+		['<C-p>'] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.select_prev_item({ behavior = 'insert' })
+			else
+				cmp.complete()
+			end
+		end),
+		['<C-n>'] = cmp.mapping(function()
+			if cmp.visible() then
+				cmp.select_next_item({ behavior = 'insert' })
+			else
+				cmp.complete()
+			end
+		end),
+	},
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
 })
--- nvim/.config/nvim/lua/lz.lua
+++ nvim/.config/nvim/lua/lz.lua
@@ -27,4 +27,6 @@ require("lazy").setup({
 	{'hrsh7th/cmp-nvim-lsp'},
 	{'hrsh7th/nvim-cmp'},
 	{'L3MON4D3/LuaSnip'},
+
+	{ "mfussenegger/nvim-jdtls" },
 })
