b1be3db3c0aa6c763f0e0f94cf5a2e2a2fac1c3d
mete <mete@localhost.localdomain>
2024-06-27 12:15:20 +0200

    clangd

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -10,43 +10,46 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = { "clangd" },
+	ensure_installed = { "clangd", "bash-language-server", "jdtls", "java-language-server", "lua-language-server"},
 	handlers = {
 		--  function(jdtls)
-		--   require('lspconfig')[jdtls].setup({})
-		-- end,
-		jdtls = lsp_zero.noop,
-	},
-})
-local cmp = require('cmp')
+			--   require('lspconfig')[jdtls].setup({})
+			-- end,
+			function(clangd)
+				require('lspconfig')[clangd].setup({})
+			end,
+			jdtls = lsp_zero.noop,
+		},
+	})
+	local cmp = require('cmp')
 
-cmp.setup({
-	sources = {
-		{ name = 'nvim_lsp' },
-	},
-	mapping = {
-		['<Enter>'] = cmp.mapping.confirm({ select = false }),
-		['<Esc>'] = cmp.mapping.abort(),
-		['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
-		['<Tab>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
-		['<C-p>'] = cmp.mapping(function()
-			if cmp.visible() then
-				cmp.select_prev_item({ behavior = 'insert' })
-			else
-				cmp.complete()
-			end
-		end),
-		['<C-n>'] = cmp.mapping(function()
-			if cmp.visible() then
-				cmp.select_next_item({ behavior = 'insert' })
-			else
-				cmp.complete()
-			end
-		end),
-	},
-	snippet = {
-		expand = function(args)
-			require('luasnip').lsp_expand(args.body)
-		end,
-	},
-})
+	cmp.setup({
+		sources = {
+			{ name = 'nvim_lsp' },
+		},
+		mapping = {
+			['<Enter>'] = cmp.mapping.confirm({ select = false }),
+			['<Esc>'] = cmp.mapping.abort(),
+			['<S-Tab>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+			['<Tab>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+			['<C-p>'] = cmp.mapping(function()
+				if cmp.visible() then
+					cmp.select_prev_item({ behavior = 'insert' })
+				else
+					cmp.complete()
+				end
+			end),
+			['<C-n>'] = cmp.mapping(function()
+				if cmp.visible() then
+					cmp.select_next_item({ behavior = 'insert' })
+				else
+					cmp.complete()
+				end
+			end),
+		},
+		snippet = {
+			expand = function(args)
+				require('luasnip').lsp_expand(args.body)
+			end,
+		},
+	})
